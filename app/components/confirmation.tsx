b5560dd83f21b40b48ddb2d0dc3f015c449bb2f8
Hussien <Adienbob@yahoo.com>
2025-07-24 21:21:17 +0300

    style: Make Confirmation message reponsive up to 480px style: The whole project is responsive up to 480px

--- app/components/confirmation.tsx
+++ app/components/confirmation.tsx
@@ -22,11 +22,11 @@ export default function Confirmation({total}: confirmationProps) {
                <picture>
                   <source media="(min-width:1024px)" srcSet={product.image.desktop} />
                   <source media="(min-width:768px)" srcSet={product.image.tablet} />
-                  <Image width={50} height={50} src={product.image.mobile} alt={product.name} />
+                  <img src={product.image.mobile} alt="" />
                </picture>
                <div className="itemDetails">
-                  <span>{product.name.split(" ", 1)}</span>
-                  <p>$ {product.price.toLocaleString()}</p>
+                  <span className="name">{product.name.split(" ", 1)}</span>
+                  <p className="price">$ {product.price.toLocaleString()}</p>
                </div>
             </div>
             <p className="quantity">x{cartItems[0].quantity}</p>
@@ -50,8 +50,8 @@ export default function Confirmation({total}: confirmationProps) {
                         <img src={product.image.mobile} alt="" />
                      </picture>
                      <div className="itemDetails">
-                        <span>{product.name.split(" ", 1)}</span>
-                        <p>$ {product.price.toLocaleString()}</p>
+                        <span className="name">{product.name.split(" ", 1)}</span>
+                        <p className="price">$ {product.price.toLocaleString()}</p>
                      </div>
                   </div>
                   <p className="quantity">x{item.quantity}</p>
@@ -69,7 +69,7 @@ export default function Confirmation({total}: confirmationProps) {
             <Image className="icon" width={64} height={64} src="/assets/checkout/icon-order-confirmation.svg" alt=""  />
             <h3>THANK YOU <br /> FOR YOUR ORDER</h3>
             <p>You will receive an email confirmation shortly.</p>
-            <div className="details">
+            <div className="container">
                <div className="items">
                {cartItems.length === 0 ? (
                   <div className="cartEmpty">
@@ -79,13 +79,13 @@ export default function Confirmation({total}: confirmationProps) {
                ) : signleProduct ? <SigleProduct/> : <AllProducts/> }
                {cartItems.length > 1 ? <button onClick={() => setSignleProduct(!signleProduct)}>{signleProduct ? `and ${cartItems.length - 1} other item(s)` : "View less"}</button> : ""}
                </div>
-            <div className="grandTotal">
-               <p>GRAND TOTAL</p>
-               <span>$ {total.toLocaleString()}</span>
+               <div className="grandTotal">
+                  <p>GRAND TOTAL</p>
+                  <span>$ {total.toLocaleString()}</span>
+               </div>
+               <Link href="/" onClick={clearCart} className="btnOrange">BACK TO HOME</Link>
             </div>
-            <Link href="/" onClick={clearCart} className="btnOrange">BACK TO HOME</Link>
          </div>
       </div>
-   </div>
    )
 }
